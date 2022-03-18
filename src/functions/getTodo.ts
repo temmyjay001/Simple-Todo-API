114588b80bd31e9726e2f3566067a8f0cbac0764
Temitayo Ajayi <temmyjay@curacel.ai>
2022-03-18 14:52:41 +0100

    move getTodo function from handlers to it's own file

---
+++ src/functions/getTodo.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import handleError from "../core/handleError";
+import jsonResponse from "../core/jsonResponse";
+import todoService from "../database/services";
+
+export const handler =  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+      const todo = todoService.getTodo(event.pathParameters?.id as string)
+      return jsonResponse(200, todo)
+  } catch(error) {
+    return handleError(error)
+  }
+}
