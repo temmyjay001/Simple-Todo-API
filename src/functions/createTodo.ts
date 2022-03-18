8eb947a23e9608faad7af8fe35224e48026d175d
Temitayo Ajayi <temmyjay@curacel.ai>
2022-03-18 14:28:45 +0100

    add validation to create todo endpoint

--- src/functions/createTodo.ts
+++ src/functions/createTodo.ts
@@ -1,26 +1,30 @@
 import { v4 } from "uuid";
 import { APIGatewayProxyResult, APIGatewayProxyEvent } from "aws-lambda";
-import { Handler } from "aws-lambda";
-import middify from "../core/middify";
 import todoService from "../database/services";
 import createTodo from "../dtos/CreateTodo";
 import jsonResponse from "../core/jsonResponse";
 import handleError from "../core/handleError";
+import * as yup from "yup";
 
-export const handler: Handler = middify(
-  async (event: APIGatewayProxyEvent & createTodo): Promise<APIGatewayProxyResult> => {
-    const { label, completed } = event.body;
+export const handler = async (event: APIGatewayProxyEvent & createTodo): Promise<APIGatewayProxyResult> => {
+  try {
+    const { label, completed } = JSON.parse(event.body as string);
+    await yup
+      .object()
+      .shape({
+        label: yup.string().required(),
+        completed: yup.bool().required(),
+      })
+      .validate(event.body, { abortEarly: false });
 
-    try {
-      const todo = await todoService.createTodo({
-        id: v4(),
-        label,
-        completed,
-        createdAt: new Date().toISOString()
-      });
-      return jsonResponse(201, todo);
-    } catch (error) {
-      return handleError(error);
-    }
-  },
-);
+    const todo = await todoService.createTodo({
+      id: v4(),
+      label,
+      completed,
+      createdAt: new Date().toISOString(),
+    });
+    return jsonResponse(201, todo);
+  } catch (error) {
+    return handleError(error);
+  }
+};
