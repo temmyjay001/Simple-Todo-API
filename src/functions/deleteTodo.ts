73e371c2686830e051bc8e0949e1f43a84d3cc28
Temitayo Ajayi <temmyjay@curacel.ai>
2022-03-19 11:03:13 +0100

    Add logic to delete a single todo

---
+++ src/functions/deleteTodo.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import handleError from "../core/handleError";
+import jsonResponse from "../core/jsonResponse";
+import todoService from "../database/services";
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    await todoService.deleteTodo(event.pathParameters?.id as string);
+    return jsonResponse(200, { success: true });
+  } catch (error) {
+    return handleError(error);
+  }
+};
