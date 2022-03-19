fa1d8655418433d6e4d3d55515064a4946f7633d
Temitayo Ajayi <temmyjay@curacel.ai>
2022-03-19 10:46:43 +0100

    Add content type as json to json response format

--- src/core/jsonResponse.ts
+++ src/core/jsonResponse.ts
@@ -1,13 +1,15 @@
 const jsonResponse = (statusCode: number, response: any): JsonResponseFormat => {
   return {
+    headers: { "Content-Type": "application/json" },
     statusCode,
     body: JSON.stringify(response),
   };
 };
 
 interface JsonResponseFormat {
-    statusCode: number;
-    body: any
-};
+  headers: any;
+  statusCode: number;
+  body: any;
+}
 
 export default jsonResponse;
