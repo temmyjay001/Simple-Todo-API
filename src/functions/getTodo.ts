68243f42a8f8ea24f7f1b505631bc2e3a7827d25
Temitayo Ajayi <temmyjay@curacel.ai>
2022-03-19 12:26:59 +0100

    await response for todo service for getting a single todo

--- src/functions/getTodo.ts
+++ src/functions/getTodo.ts
@@ -3,9 +3,9 @@ import handleError from "../core/handleError";
 import jsonResponse from "../core/jsonResponse";
 import todoService from "../database/services";
 
-export const handler =  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-      const todo = todoService.getTodo(event.pathParameters?.id as string)
+      const todo = await todoService.getTodo(event.pathParameters?.id as string)
       return jsonResponse(200, todo)
   } catch(error) {
     return handleError(error)
